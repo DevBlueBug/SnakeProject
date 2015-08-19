66041930f2bcc600e4e2a0aa9b25ec7abdc1c6fd
DevBlueBug <devbluebug@gmail.com>
2015-08-19 11:02:23 +0900

    Tiles initialization added.
    none.

--- Assets/Script/Game/World.cs
+++ Assets/Script/Game/World.cs
@@ -10,6 +10,9 @@ namespace NWorld{
 			this.w = w;
 			this.h = h;
 			tiles = new Tile[w, h];
+			for (int i = 0; i < w; i++) for(int j = 0 ; j < h;j++){
+				tiles[i,j] = new Tile();
+			}
 
 		}
 	}
